b30814fc8473dde87cc81f01eb9d208ddb4aae2d
Oscar Terceros <osterce@gmail.com>
2021-06-18 22:34:50 -0400

    TodoList eliminarTodo()

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import { saludar } from './js/componentes';
 import './styles.css';
 
-const nombre = 'Oscar';
+import { Todo, TodoList } from './classes'
+import { crearTodoHtml } from './js/componentes';
 
-saludar( nombre );
+export const todoList = new TodoList();
+const tarea = new Todo( 'Aprender JavaScript' );
+
+todoList.nuevoTodo( tarea );
+
+console.log( todoList );
+
+crearTodoHtml( tarea );
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,18 +1,58 @@
-import '../css/componentes.css';
-/* import webpacklogo from '../assets/img/webpack-logo.png'; */
+import { todoList } from "..";
+import { Todo } from "../classes";
 
-export const saludar = ( nombre ) => {
+//Referencias HTML
+const divTodoList = document.querySelector('.todo-list');
+const txtInput = document.querySelector('.new-todo');
 
-  console.log('Creando etiqueta h1');
-  const h1 = document.createElement('h1');
-  h1.innerText = `Hola ${nombre}`;
+export const crearTodoHtml = ( todo ) => {
 
-  document.body.append( h1 );
+  const htmlTodo = `
+    <li class="${ ( todo.completado ) ? 'completed' : ' ' }" data-id="${ todo.id }">
+      <div class="view">
+        <input class="toggle" type="checkbox" ${ ( todo.completado ) ? 'checked' : ''}>
+        <label>${ todo.tarea }</label>
+        <button class="destroy"></button>
+      </div>
+      <input class="edit" value="Create a TodoMVC template">
+    </li>
+  `;
+  const div = document.createElement( 'div' );
+  div.innerHTML = htmlTodo;
 
-  //Img
-  /* console.log(webpacklogo);
-  const img = document.createElement( 'img' );
-  img.src = webpacklogo;
-  document.body.append( img ); */
-  
+  divTodoList.append( div.firstElementChild );
+
+  return div.firstElementChild;
 }
+
+//Eventos
+
+txtInput.addEventListener( 'keyup', ( event ) =>{
+
+  if( event.keyCode === 13 && txtInput.value.length > 0) {
+
+    const nuevoTodo = new Todo( txtInput.value );
+    todoList.nuevoTodo( nuevoTodo );
+    crearTodoHtml( nuevoTodo );
+    txtInput.value = '';
+
+  }
+})
+
+divTodoList.addEventListener('click', ( event ) =>{
+  
+  const nombreElemento = event.target.localName; //input, label, button
+  const todoElemento = event.target.parentElement.parentElement;
+  const todoId = todoElemento.getAttribute( 'data-id' );
+
+  if (nombreElemento.includes( 'input' ) ) {
+    todoList.marcarCompletado( todoId );
+    todoElemento.classList.toggle('completed');
+  } else if( nombreElemento.includes( 'button' ) ) {
+    todoList.eliminarTodo( todoId );
+    divTodoList.removeChild( todoElemento );
+  }
+  
+  console.log(todoList);
+})
+
