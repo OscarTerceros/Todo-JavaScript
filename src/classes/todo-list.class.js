1f896971767af54e9895f84ccb6932d6027ec730
Oscar Terceros <osterce@gmail.com>
2021-06-22 00:26:12 -0400

    Implementando LocalStorage y restaurando los valores del todoList como instancias

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -1,29 +1,50 @@
+import { Todo } from "./todo.class";
 
 export class TodoList {
   constructor() {
-    this.todos = [];
+    //this.todos = [];
+    this.cargarLocalStorage();
   }
 
-  nuevoTodo( todo ) {
-    this.todos.push( todo );
+  nuevoTodo(todo) {
+    this.todos.push(todo);
+    this.guardarLocalStorage();
   }
 
-  eliminarTodo( id ) {
-    this.todos = this.todos.filter( todo => todo.id != id);
+  eliminarTodo(id) {
+    this.todos = this.todos.filter(todo => todo.id != id);
+    this.guardarLocalStorage();
   }
 
-  marcarCompletado( id ) {
+  marcarCompletado(id) {
 
-    for( const todo of this.todos) {
+    for (const todo of this.todos) {
 
-      if( todo.id == id) {
+      if (todo.id == id) {
         todo.completado = !todo.completado;
+        this.guardarLocalStorage();
         break;
       }
     }
   }
 
   eliminarCompletado() {
-    this.todos = this.todos.filter( todo => !todo.completado );
+    this.todos = this.todos.filter(todo => !todo.completado);
+    this.guardarLocalStorage();
+  }
+
+  guardarLocalStorage() {
+    localStorage.setItem('todo', JSON.stringify(this.todos)); //convertir un objeto a JSON
+  }
+
+  cargarLocalStorage() {
+
+    this.todos = (localStorage.getItem('todo'))
+      ? JSON.parse(localStorage.getItem('todo'))
+      : [];
+
+    //this.todos = this.todos.map(obj => Todo.fromJson(obj));
+    this.todos = this.todos.map(Todo.fromJson);
+
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import { Todo, TodoList } from './classes'
 import { crearTodoHtml } from './js/componentes';
 
 export const todoList = new TodoList();
-const tarea = new Todo( 'Aprender JavaScript' );
 
-todoList.nuevoTodo( tarea );
+//todoList.todos.forEach( todo => crearTodoHtml( todo )); //si es un solo argumento se resume
+todoList.todos.forEach(crearTodoHtml);
 
-console.log( todoList );
-
-crearTodoHtml( tarea );
+console.log(todoList.todos);
